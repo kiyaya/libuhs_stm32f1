9d62fbfda5970079d411a18a1ac9a4e9f677541c
kiya <kiyaya@gmail.com>
2014-08-26 10:21:39 +0800

    Add generated infomation for debug.

---
+++ prj/debug/sources.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ELF_SRCS := 
+O_SRCS := 
+CPP_SRCS := 
+C_UPPER_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+OBJ_SRCS := 
+ASM_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
+CC_SRCS := 
+OBJS := 
+C_DEPS := 
+SECONDARY_FLASH := 
+CXX_DEPS := 
+S_UPPER_DEPS := 
+SECONDARY_LIST := 
+SECONDARY_SIZE := 
+C++_DEPS := 
+ASM_DEPS := 
+CC_DEPS := 
+CPP_DEPS := 
+EXECUTABLES := 
+C_UPPER_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+library/usb \
+library/fat_sd \
+library/fat_sd/option \
+cmsis/device \
+cmsis/core \
+cmsis/STM32F10x_StdPeriph_Driver/src \
+bsp \
+bsp/bsp_usb_otg/src \
+app \
+
